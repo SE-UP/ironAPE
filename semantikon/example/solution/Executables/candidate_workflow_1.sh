825aa8a4c11f3d90cf2cbaa221d7d24255e8205c
Charlotte Grunert <cgrunert21@gmail.com>
2026-02-05 12:28:13 +0100

    feat: adjusted semantikon example

---
+++ semantikon/example/solution/Executables/candidate_workflow_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -ne 3 ]
+	then
+		echo "3 argument(s) expected."
+		exit
+fi
+node659340092=$1
+node-1714624059=$2
+node-542497505=$3
+
+"Error. Tool 'get_speed' is missing the execution code."
+"Error. Tool 'get_kinetic_energy' is missing the execution code."
+echo "1. output is: $node1493707640"
